ee80f49e66731b51e85708f66c0c1d50c6a20258
praseedm <prasi.praseed@gmail.com>
2017-04-29 20:07:50 +0530

    initial_commit

---
+++ Gui.py
@@ -0,0 +1,101 @@
+
+from Tkinter import *
+master = Tk()
+
+
+Width = 100
+(x, y) = (5, 5)
+actions = ["up", "down", "left", "right"]
+
+board = Canvas(master, width=x*Width, height=y*Width)
+player = (0, y-1)
+score = 1
+restart = False
+walk_reward = -0.1
+
+walls = [(1, 1), (1,3), (1,4), (1, 2), (2, 2) , (3,0) , (4,2) ]
+specials = [(4, 0, "red", 10)]
+
+
+
+
+
+def render_grid():
+    global specials, walls, Width, x, y, player
+    for i in range(x):
+        for j in range(y):
+            board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="white", width=1)
+            temp = {}
+    for (i, j, c, w) in specials:
+        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill=c, width=1)
+    for (i, j) in walls:
+        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="black", width=1)
+
+render_grid()
+
+
+
+
+
+def try_move(dx, dy):
+    global player, x, y, score, walk_reward, agent, restart
+    if restart == True:
+        restart_game()
+    new_x = player[0] + dx
+    new_y = player[1] + dy
+    score += walk_reward
+    if (new_x >= 0) and (new_x < x) and (new_y >= 0) and (new_y < y) and not ((new_x, new_y) in walls):
+        board.coords(agent, new_x*Width+Width*2/10, new_y*Width+Width*2/10, new_x*Width+Width*8/10, new_y*Width+Width*8/10)
+        player = (new_x, new_y)
+    for (i, j, c, w) in specials:
+        if new_x == i and new_y == j:
+            score -= walk_reward
+            score += w
+            if score > 0:
+                print "Goal! : ", score
+           # else:
+            #    print "Goal! : ", score
+            restart = True
+            return
+    #print "score: ", score
+
+
+def call_up(event):
+    try_move(0, -1)
+
+
+def call_down(event):
+    try_move(0, 1)
+
+
+def call_left(event):
+    try_move(-1, 0)
+
+
+def call_right(event):
+    try_move(1, 0)
+
+
+def restart_game():
+    global player, score, agent, restart
+    player = (0, y-1)
+    score = 1
+    restart = False
+    board.coords(agent, player[0]*Width+Width*2/10, player[1]*Width+Width*2/10, player[0]*Width+Width*8/10, player[1]*Width+Width*8/10)
+
+def has_restarted():
+    return restart
+
+master.bind("<Up>", call_up)
+master.bind("<Down>", call_down)
+master.bind("<Right>", call_right)
+master.bind("<Left>", call_left)
+
+agent = board.create_rectangle(player[0]*Width+Width*2/10, player[1]*Width+Width*2/10,
+                            player[0]*Width+Width*8/10, player[1]*Width+Width*8/10, fill="yellow", width=1, tag="agent")
+
+board.grid(row=0, column=0)
+
+
+def start_game():
+    master.mainloop()
---
+++ Learner.py
@@ -0,0 +1,143 @@
+
+import Gui
+import threading
+import time
+import csv
+import pickle
+import os
+
+discount = 0.8
+actions = Gui.actions
+states = []
+Q = {}
+
+
+
+for i in range(Gui.x):
+    for j in range(Gui.y):
+        states.append((i, j))
+
+
+def init_Q() :
+	for state in states:
+		temp = {}
+		for action in actions:
+			temp[action] = 0.1
+		Q[state] = temp
+
+#Read Q
+if os.path.isfile('data.pkl'):
+	pkl_file = open('data.pkl', 'rb')
+	Q = pickle.load(pkl_file) 
+	#print Q[(0,4)] 
+	print "READ\n" 
+else :
+	init_Q()
+	print "Initialize\n" 
+
+for wall in Gui.walls:
+	temp = {}
+	for action in actions:
+		temp[action] = -1
+	Q[wall] = temp  
+
+for (i, j, c, w) in Gui.specials:
+    for action in actions:
+        Q[(i, j)][action] = w
+        
+
+
+def do_action(action):
+    s = Gui.player
+    r = -Gui.score
+    if action == actions[0]:
+        Gui.try_move(0, -1)
+    elif action == actions[1]:
+        Gui.try_move(0, 1)
+    elif action == actions[2]:
+        Gui.try_move(-1, 0)
+    elif action == actions[3]:
+        Gui.try_move(1, 0)
+    else:
+        return
+    s2 = Gui.player
+    r += Gui.score
+    return s, action, r, s2
+
+#Find Optimal action for a state
+def max_Q(s):
+    val = None
+    act = None
+    for a, q in Q[s].items():
+        if val is None or (q > val):
+            val = q
+            act = a
+    return act, val
+
+
+def inc_Q(s, a, alpha, inc):
+    Q[s][a] *= 1 - alpha
+    Q[s][a] += alpha * inc
+    
+    
+def printq():
+	pkl_file = open('data.pkl', 'wb+')
+	pickle.dump(Q, pkl_file)
+	target = open('dict_2.csv', 'wb')
+	writer = csv.writer(target)
+	for i in Q.keys():
+		writer.writerow([i,Q[i]])
+	target.close();
+	
+		     
+
+
+def run():
+    global discount
+    time.sleep(1)
+    alpha = 1
+    t = 1
+    count = 0
+    ff = 0
+    
+    printq()
+    
+    while True:
+    	i= 0
+    	ff += 1
+        # Pick  action
+        s = Gui.player
+        #print Q[(0,4)] 
+        max_act, max_val = max_Q(s)
+        (s, a, r, s2) = do_action(max_act)
+        
+		
+        # Update Q
+        max_act, max_val = max_Q(s2)
+        inc_Q(s, a, alpha, r + discount * max_val)
+        printq()
+
+        #  restarted
+        t += 1.0
+        if Gui.has_restarted():
+        	count += 1
+        	Gui.restart_game()
+        	time.sleep(0.01)
+        	t = 1.0
+        	print "\nIteration = " , count
+        	'''if count == 2 :
+        		break'''
+            
+
+        # Update the learning rate
+        alpha = pow(t, -0.1)
+
+        # SLEEP.
+        time.sleep(0.01)
+        
+
+
+t = threading.Thread(target=run)
+t.daemon = True
+t.start()
+Gui.start_game()
